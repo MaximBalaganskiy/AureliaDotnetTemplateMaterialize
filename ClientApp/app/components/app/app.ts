377a4b0f6442cf66fa579001aafce38514c6ecd5
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2018-10-30 10:26:47 +1100

    Replace Bootstrap with Materialize

--- ClientApp/app/components/app/app.ts
+++ ClientApp/app/components/app/app.ts
@@ -7,21 +7,21 @@ export class App {
 		config.map([{
 			route: ["", "home"],
 			name: "home",
-			settings: { icon: "fa fa-home" },
+			settings: { icon: "home" },
 			moduleId: "../home/home",
 			nav: true,
 			title: "Home"
 		}, {
 			route: "counter",
 			name: "counter",
-			settings: { icon: "fa fa-plus" },
+			settings: { icon: "add" },
 			moduleId: "../counter/counter",
 			nav: true,
 			title: "Counter"
 		}, {
 			route: "fetch-data",
 			name: "fetchdata",
-			settings: { icon: "fa fa-list" },
+			settings: { icon: "list" },
 			moduleId: "../fetchdata/fetchdata",
 			nav: true,
 			title: "Fetch data"
